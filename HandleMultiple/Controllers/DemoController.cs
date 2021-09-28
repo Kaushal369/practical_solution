ac4d609d6affcc2d47d89d1fa669af727f4ffed6
kaushal369 <kaushal369@gmail.com>
2021-09-28 15:08:33 +0530

    add calculation action method and perform handle multiple button on Index view at server side.

--- HandleMultiple/Controllers/DemoController.cs
+++ HandleMultiple/Controllers/DemoController.cs
@@ -13,5 +13,44 @@ namespace HandleMultiple.Controllers
         {
             return View();
         }
+        
+        public ActionResult Calculation()
+        {
+            float num1=0;
+            float num2=0;
+            try
+            {
+                 num1 = float.Parse(Request["Num1"]);
+
+                num2 = float.Parse(Request["Num2"]);
+
+                float result = 0;
+
+                if (Request["Command"] == "Add")
+                    result = num1 + num2;
+                else if (Request["Command"] == "Sub")
+                    result = num1 - num2;
+
+                else if (Request["Command"] == "Mul")
+                    result = num1 * num2;
+
+                else if (Request["Command"] == "Div")
+                {
+                    if (num2 == 0)
+                        throw new DivideByZeroException("You Cann't divide by zero");
+                    result = num1 / num2;
+                }
+                    
+
+                ViewBag.results = result.ToString();
+            }
+            catch (Exception ex)
+            {
+               
+                    throw;
+            }  
+            
+            return View("~/Views/Demo/Index.cshtml");
+        }
     }
 }
